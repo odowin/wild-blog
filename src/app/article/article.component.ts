c2fdbf13912ff18fca6f341fc50466189be3b7e1
odowin <102875944+odowin@users.noreply.github.com>
2024-09-25 21:41:17 +0200

    Améliorer l'affichage de l'article

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss'
 })
 export class ArticleComponent {
+  article = {
+    title: 'Titre de l\'article',
+    author: 'John Doe',
+    content: 'Voici le contenu de l\'article.',
+    image: 'https://via.placeholder.com/350x150',
+    isPublished: true,
+    comment: ''
+  };
 
+  // Fonction qui inverse l'état de publication de l'article
+  togglePublication(): void {
+    this.article.isPublished = !this.article.isPublished;
+  }
 }
