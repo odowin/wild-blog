467c3f1fa432004de833d8b679d8dcfb2f65fc98
odowin <102875944+odowin@users.noreply.github.com>
2024-10-04 10:05:45 +0200

    home page ok

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ArticleComponent } from "../article/article.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [ArticleComponent],
+  imports: [ArticleComponent, CommonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
@@ -17,8 +18,14 @@ export class HomePageComponent {
   ];
 
   likedArticle: string | null = null; // Propriété pour stocker l'article liké
+  isShown: boolean = false; // Initialisation de la variable pour gérer la visibilité de la notification
 
   onArticleLiked(title: string) {
     this.likedArticle = title; // Stocke le titre de l'article liké
+    this.isShown = true; // Affiche la notification
+    setTimeout(() => {
+      this.isShown = false; // Masque la notification après un certain temps
+    }, 3000); // Durée avant de cacher la notification
   }
+
 }
